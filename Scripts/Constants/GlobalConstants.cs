f3780d59191e07850cf71ed8d9a7cfe7e6507703
Simone Rizzuto <simorico.dev@gmail.com>
2025-01-24 21:00:45 +1000

    added collision logic + sphere + main

---
+++ Scripts/Constants/GlobalConstants.cs
@@ -0,0 +1,5 @@
+namespace GGJ2025.Scripts.Constants;
+public static class GlobalConstants
+{
+    public static string PlayerName = "Player";
+}
--- Scripts/MessageBubble/MessageBubble.cs
+++ Scripts/MessageBubble/MessageBubble.cs
@@ -1,3 +1,4 @@
+using GGJ2025.Scripts.Constants;
 using Godot;
 
 namespace GGJ2025.Scripts.MessageBubble;
@@ -5,4 +6,17 @@ public partial class MessageBubble : Node3D
 {
     [Export(PropertyHint.MultilineText)] public string Message;
     [Export] public string ProfilePicPath;
+
+    private void _on_area_3d_body_entered(Node3D bodyEntered)
+    {
+        var isPlayer = bodyEntered is CharacterBody3D player && player.Name == GlobalConstants.PlayerName;
+        if (isPlayer)
+        {
+            // trigger dialogue UI, pass in Message and ProfilePicPath
+            
+            GD.Print("trigger message display");
+            
+            return;
+        }
+    }
 }
