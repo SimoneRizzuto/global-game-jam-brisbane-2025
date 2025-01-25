74054c127b87442ce931a3d4688ab3572313af44
Simone Rizzuto <simorico.dev@gmail.com>
2025-01-25 12:37:44 +1000

    changes things

--- Scripts/Modules/Enemies/EnemyPatrollingModule.cs
+++ Scripts/Modules/Enemies/EnemyPatrollingModule.cs
@@ -1,3 +1,4 @@
+using GGJ2025.Scripts.Enemies;
 using GGJ2025.Scripts.StateMachines;
 using Godot;
 
@@ -7,5 +8,9 @@ namespace GGJ2025.Scripts.Modules.Enemies;
 public partial class EnemyPatrollingModule : Node
 {
     public EnemyStateMachine State => GetParent().GetParent<EnemyStateMachine>();
-    
+
+    public override void _Process(double delta)
+    {
+        if (State.EnemyState != EnemyState.Patrolling) return;
+    }
 }
--- Scripts/Modules/Enemies/EnemyResettingModule.cs
+++ Scripts/Modules/Enemies/EnemyResettingModule.cs
@@ -1,3 +1,4 @@
+using GGJ2025.Scripts.Enemies;
 using GGJ2025.Scripts.StateMachines;
 using Godot;
 
@@ -7,4 +8,9 @@ namespace GGJ2025.Scripts.Modules.Enemies;
 public partial class EnemyResettingModule : Node
 {
     public EnemyStateMachine State => GetParent().GetParent<EnemyStateMachine>();
+
+    public override void _Process(double delta)
+    {
+        if (State.EnemyState != EnemyState.Resetting) return;
+    }
 }
